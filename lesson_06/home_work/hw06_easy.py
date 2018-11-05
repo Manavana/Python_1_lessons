95bc457b6a9377a31391f68311d560b2c8830044
Manavana <stivrina.maria@gmail.com>
2018-11-06 01:41:26 +0700

    Дополнено решение задач уровня easy

--- lesson_06/home_work/hw06_easy.py
+++ lesson_06/home_work/hw06_easy.py
@@ -4,7 +4,7 @@ __author__ = "Стиврина Мария"
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 import math
 
-class Figure:
+class Triangle:
     def __init__(self, x1, y1, x2, y2, x3, y3):
         self.x1 = x1
         self.y1 = y1
@@ -13,63 +13,66 @@ class Figure:
         self.x3 = x3
         self.y3 = y3
 
-    def side_length(self):
+    def side1_length(self):
         side1 = math.sqrt((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2)
+        return side1
+
+    def side2_length(self):
         side2 = math.sqrt((self.x3 - self.x2) ** 2 + (self.y3 - self.y2) ** 2)
+        return side2
+
+    def side3_length(self):
         side3 = math.sqrt((self.x1 - self.x3) ** 2 + (self.y1 - self.y3) ** 2)
-        return side1, side2, side3
+        return side3
 
     def perimeter(self):
-        perimeter_of_the_figure = side1 + side2 + side3
+        perimeter_of_the_figure = triangle.side1_length() + triangle.side2_length() + triangle.side3_length()
         return perimeter_of_the_figure
 
     def area(self):
-        p = perimeter_of_the_figure / 2
-        figure_area = sqrt(p * (p - side1) * (p - side2) * (p - side3))
+        p = triangle.perimeter() / 2
+        figure_area = math.sqrt(p * (p - triangle.side1_length()) * (p - triangle.side2_length()) * (p -
+        triangle.side3_length()))
         return figure_area
 
     def height(self):
-        triangle_height = (2 * figure_area) / side1
+        triangle_height = (2 * triangle.area()) / triangle.side1_length()
         return triangle_height
 
-triangle = Figure(-5, -2, 4, 5, 6, -1)
-print(triangle.perimeter())
-print(triangle.area())
-print(triangle.height())
+triangle = Triangle(-5, -2, 4, 5, 6, -1)
+print("Периметр треугольника: ", triangle.perimeter())
+print("Площадь треугольника: ", triangle.area())
+print("Высота треугольника: ", triangle.height())
 
-
-"""class Student(People):
-    def __init__(self, name, surname, birth_date, school, class_room):
-        # Явно вызываем конструктор родительского класса
-        People.__init__(self, name, surname, birth_date, school)
-        # Добавляем уникальные атрибуты
-        self._class_room = {'class_num': int(class_room.split()[0]),
-                            'class_char': class_room.split()[1]}
-
-    # И уникальные методы
-    @property
-    def class_room(self):
-        return "{} {}".format(self._class_room['class_num'], self._class_room['class_char'])
-
-    def next_class(self):
-        self._class_room['class_num'] += 1
-
-
-class Teacher(People):
-    def __init__(self, name, surname, birth_date, school, teach_classes):
-        People.__init__(self, name, surname, birth_date, school)
-        self.teach_classes = list(map(self.convert_class, teach_classes))
-
-    # Уникальный метод Учителя
-    def convert_class(self, class_room):
-        
-        '<class_num> <class_int>' --> {'class_num': <class_num>, 'class_int': <class_int>}
-        
-        return {'class_num': int(class_room.split()[0]),
-                'class_char': class_room.split()[1]}
-"""
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class Trapezium(Triangle):
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        Triangle.__init__(self, x1, y1, x2, y2, x3, y3)
+        self.x4 = x4
+        self.y4 = y4
+
+    def side3_length(self):
+        side3 = math.sqrt((self.x4 - self.x3) ** 2 + (self.y4 - self.y3) ** 2)
+        return side3
+
+    def side4_length(self):
+        side4 = math.sqrt((self.x1 - self.x4) ** 2 + (self.y1 - self.y4) ** 2)
+        return side4
+
+    def perimeter(self):
+        perimeter_of_the_figure = trapezium.side1_length() + trapezium.side2_length() + trapezium.side3_length()
+        + trapezium.side4_length()
+        return perimeter_of_the_figure
+
+    def area(self):
+        p = triangle.perimeter() / 2
+        figure_area = math.sqrt(p * (p - triangle.side1_length()) * (p - triangle.side2_length()) * (p -
+        triangle.side3_length()))
+        return figure_area
+
+trapezium = Trapezium(1, 2, 3, 4, 5, 6, 7, 8)
+print("Периметр трапеции: ", trapezium.perimeter())
