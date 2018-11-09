40ceeffd608639c977e31d4e3175eadd990577eb
Manavana <stivrina.maria@gmail.com>
2018-11-10 01:32:05 +0700

    Создала класс сумки с бочонками. Реализовала алгоритм игры. (пока без вычеркивания цифр из карточек)

--- lesson_07/home_work/loto.py
+++ lesson_07/home_work/loto.py
@@ -71,18 +71,22 @@ class Card:
 
         # Прописываю в первом цикле for генерацию значений для карточки больше 15 + отбираю неповторяющиеся
         for el in range(20):
-            element = random.randint(1, 90)
+            element = random.randint(1, 91)
             if element not in intermediate_list:
                 intermediate_list.append(element)
 
         # отбираю нужное количество неповторяющихся значений для карточки
         self.card = random.sample(intermediate_list, 15)
+        #print(self.card)
         self.line1 = self.card[:5]
         self.line1.sort()
         self.line2 = self.card[5:10]
         self.line2.sort()
         self.line3 = self.card[10:]
         self.line3.sort()
+
+    def get_len_card(self):
+        return len(self.card)
 
     def __str__(self):
         return """{}  {}  {}  {}  {}
@@ -91,10 +95,72 @@ class Card:
                              self.line2[0], self.line2[1], self.line2[2], self.line2[3], self.line2[4],
                              self.line3[0], self.line3[1], self.line3[2], self.line3[3], self.line3[4])
 
+    def compare_numbers(self, number):
+        for num in self.card:
+            if num == number:
+                return True
+        return False
+
+class Barrel:
+    def __init__(self):
+        self.barrel_in_bag = []
+        self.barrel_dropped = []
+
+    def shuffle_bag(self):
+        # накидали бочонков в мешок и тщательно перемешали
+        self.barrel_in_bag = [i for i in range(1, 91)]
+        random.shuffle(self.barrel_in_bag)
+
+    def get_barrel(self):
+        # вытянули случайный бочонок из мешка
+        self.barrel = random.choice(self.barrel_in_bag)
+        # скинули его в другой мешок
+        self.barrel_dropped.append(self.barrel)
+        index_barrel = self.barrel_in_bag.index(self.barrel)
+        self.barrel_in_bag.pop(index_barrel)
+        print("\nНовый бочонок: ", self.barrel, "(осталось {})".format(len(self.barrel_in_bag)))
+        return self.barrel
+
 my_card = Card()
 my_card.make_card()
 
 computer_card = Card()
 computer_card.make_card()
 
-print(my_card)
+bag_with_barrel = Barrel()
+bag_with_barrel.shuffle_bag()
+
+while my_card.get_len_card() > 0 and computer_card.get_len_card() > 0:
+    if my_card.get_len_card() == 0:
+        print("$$$ Поздравляем! Вы выиграли джекпот! $$$")
+        break
+
+    if computer_card.get_len_card() == 0:
+        print("Сегодня не Ваш день...")
+        break
+
+    number = bag_with_barrel.get_barrel()
+
+    print("\n------ Моя карточка ------")
+    print(my_card)
+    print("--------------------------")
+
+    print("\n--- Карточка компьютера --")
+    print(computer_card)
+    print("--------------------------")
+
+    answer = input("Зачеркнуть цифру? (y/n)")
+
+    if my_card.compare_numbers(number) == True and answer == "y":
+        continue
+    elif my_card.compare_numbers(number) == True and answer == "n":
+        print("Вы ошиблись. Такая цифра есть у Вас в карточке. К сожалению, игра окончена")
+        break
+    elif my_card.compare_numbers(number) == False and answer == "y":
+        print("Вы ошиблись. Такой цифры нет у Вас в карточке. К сожалению, игра окончена")
+        break
+    elif my_card.compare_numbers(number) == False and answer == "n":
+        continue
+    else:
+        print("Я не понял Ваш ответ. Извините, но это проигрыш")
+        break
